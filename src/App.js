d8b8538452ec34eb5c709d6b048cf7b3b6128d69
Ali <ar.k@outlook.com>
2018-08-01 21:17:16 -0700

    add BookAPI to App component for backend API

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Read from './components/Read';
 import CurrentlyReading from './components/CurrentlyReading';
 import WantToRead from './components/WantToRead';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import * as BookAPI from './BooksAPI';
 
 library.add(fab)
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    const books = BookAPI.getAll();
+    console.log(books);
+  }
   render() {
     return (
       <div className="App">
